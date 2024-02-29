79e49322a9a0eb00fee64afbcd08aaabe2d99d9f
Arturo <arturo.gil@umh.es>
2024-02-29 17:41:22 +0100

    Solved the TRAPPED case in RRT PC connect and RRT PC connect to goal

--- keyframe/keyframe.py
+++ keyframe/keyframe.py
@@ -4,31 +4,8 @@ import open3d as o3d
 
 
 class KeyFrame():
-    def __init__(self, filename, voxel_size=0.2):
-        # self.transform = None
-        # voxel sizes
-        # self.voxel_size = voxel_size
-        # self.voxel_size_normals = 5*self.voxel_size
-        # self.voxel_size_fpfh = 5*self.voxel_size
-        # self.icp_threshold = 5
-        # self.fpfh_threshold = 5
-        # filename = directory + '/robot0/lidar/data/' + str(scan_time) + '.pcd'
-
+    def __init__(self, filename):
         self.pointcloud = o3d.io.read_point_cloud(filename, print_progress=True)
-        # downsample pointcloud and save to pointcloud in keyframe
-        # self.pointcloud = self.pointcloud.voxel_down_sample(voxel_size=self.voxel_size)
-
-
-        # calcular las normales a cada punto
-        # self.pointcloud.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=self.voxel_size_normals,
-        #                                                                       max_nn=30))
-
-        # extraer los Fast Point Feature Histograms
-        # self.pointcloud_fpfh = o3d.pipelines.registration.compute_fpfh_feature(self.pointcloud,
-        #                                                                        o3d.geometry.KDTreeSearchParamHybrid(
-        #                                                                            radius=self.voxel_size_fpfh,
-        #                                                                            max_nn=100))
-
 
     def draw_cloud(self):
         o3d.visualization.draw_geometries([self.pointcloud],
